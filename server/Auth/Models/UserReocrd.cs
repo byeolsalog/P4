809bc5dcd25d952040ad3d7d40b88ba819af967e
byeolsalog <syrup272@gmail.com>
2025-09-17 18:24:56 +0900

    서버 레포 분할

--- server/Auth/Models/UserReocrd.cs
+++
@@ -1,9 +0,0 @@
-namespace Auth.Models;
-public sealed class UserRecord
-{
-    public long Id { get; set; }
-    public string Provider { get; set; } = "";
-    public string ProviderSub { get; set; } = "";
-    public string? DisplayName { get; set; }
-    public string? Email { get; set; }
-}
